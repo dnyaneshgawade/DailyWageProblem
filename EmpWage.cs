90fa4b454ab3ae5ae12ce36cb09990b0b4952af9
dnyana <dnyaneshgawade903@gmail.com>
2021-07-20 20:50:19 +0530

    [Dnyanesh] add: attendance of employee

---
+++ EmpWage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyWageProblem
+{
+    public class EmpWage
+    {
+        public void attendance()
+        {
+            Random ran = new Random();
+            int attend = ran.Next(0, 2);
+            if (attend == 0)
+                Console.WriteLine("Employee is Present");
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace DailyWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            EmpWage emp = new EmpWage();
+            emp.attendance();
         }
     }
 }
