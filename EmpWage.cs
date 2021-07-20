d3b2d43aebd4a9eb3e0e3452587050c8f58eee96
dnyana <dnyaneshgawade903@gmail.com>
2021-07-20 21:35:18 +0530

    [Dnyanesh] add: monthly wage using switch case

--- EmpWage.cs
+++ EmpWage.cs
@@ -7,30 +7,30 @@ namespace DailyWageProblem
     public class EmpWage
     {
         const int Wage_Per_Hr = 20;
+        const int WORKING_DAYS = 20;
         int payment = 0;
         int workHr;
 
-        public void switchCase()
+        public void monthlyWage()
         {
             Random random = new Random();
             int num = random.Next(0, 3);
             switch (num)
             {
                 case 0:
-                    Console.WriteLine("daily wage of fulltime employee");
+                    Console.WriteLine("Monthly wage of fulltime employee");
                     workHr = 8;
                     break;
                 case 1:
-                    Console.WriteLine("daily wage of parttime employee");
+                    Console.WriteLine("Monthly wage of parttime employee");
                     workHr = 4;
                     break;
                 default:
                     Console.WriteLine("employee is absent");
                     break;
             }
-            payment = Wage_Per_Hr * workHr;
+            payment = Wage_Per_Hr * workHr * WORKING_DAYS;
             Console.WriteLine("Payment is:" + payment);
         }
-
     }
 }
